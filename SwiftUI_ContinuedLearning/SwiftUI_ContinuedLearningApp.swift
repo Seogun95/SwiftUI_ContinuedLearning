c7c10bc616b3e19a5e183fa38977d56fda47e8e4
SeonJungKim <ksj083895@gmail.com>
2021-05-20 17:40:23 +0500

    Initial Commit

---
+++ SwiftUI_ContinuedLearning/SwiftUI_ContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ContinuedLearningApp.swift
+//  SwiftUI_ContinuedLearning
+//
+//  Created by Seogun Kim on 2021/05/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
